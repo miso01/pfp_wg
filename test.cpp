c2fc11905baa10f186c597433b7650fcfd7d5199
andynet <andrejbalaz001@gmail.com>
2022-10-03 13:12:46 +0200

    quite nice tfm_construct

--- test.cpp
+++ test.cpp
@@ -24,30 +24,34 @@ string untunnel(tfm_index &tfm) {
     return original;
 }
 
+vector<uint64_t> get_C(int_vector<> &L, size_t sigma) {
+    // 255 handles char alphabet, sigma + 1 handles int alphabets
+    vector<uint64_t> v(max((size_t)255, sigma + 1), 0);
+    for (uint64_t i = 0; i < L.size(); i++) v[L[i] + 1] += 1;
+    for (uint64_t i = 0; i < v.size() - 1; i++) v[i + 1] += v[i];
+    return v;
+}
+
 tfm_index construct(size_t size, int_vector<> &L, bit_vector &din, bit_vector &dout) {
     tfm_index tfm;
+
     tfm.text_len = size;
-
     construct_im(tfm.m_L, L);
+    tfm.m_C = get_C(L, tfm.m_L.sigma);
+    tfm.m_dout = dout;
+    tfm.m_din = din;
 
-    tfm.m_C = vector<uint64_t>(255, 0);
-    for (uint64_t i = 0; i < L.size(); i++) tfm.m_C[L[i] + 1] += 1;
-    for (uint64_t i = 0; i < tfm.m_C.size() - 1; i++) tfm.m_C[i + 1] += tfm.m_C[i];
-
-    tfm.m_dout = tfm_index::bit_vector_type(std::move(dout));
-    sdsl::util::init_support(tfm.m_dout_rank, &tfm.m_dout);
+    sdsl::util::init_support(tfm.m_dout_rank,   &tfm.m_dout);
     sdsl::util::init_support(tfm.m_dout_select, &tfm.m_dout);
-
-    tfm.m_din = tfm_index::bit_vector_type(std::move(din));
-    sdsl::util::init_support(tfm.m_din_rank, &tfm.m_din);
-    sdsl::util::init_support(tfm.m_din_select, &tfm.m_din);
+    sdsl::util::init_support(tfm.m_din_rank,    &tfm.m_din);
+    sdsl::util::init_support(tfm.m_din_select,  &tfm.m_din);
 
     return tfm;
 }
 
 int main() {
     size_t size = 12;
-    int_vector<> L{'i', 'p', 's', 's', 'm', '$', 'p', 'i', 's', 's', 'i', 'i'};
+    int_vector<> L = {'i', 'p', 's', 's', 'm', '$', 'p', 'i', 's', 's', 'i', 'i'};
     bit_vector in{1,1,1,1,1,1,1,1,1,1,1,1,1};
     bit_vector out{1,1,1,1,1,1,1,1,1,1,1,1,1};
 
