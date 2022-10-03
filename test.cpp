5cae0e82e786e89abc5acaed08fd4e668af962ca
andynet <andrejbalaz001@gmail.com>
2022-10-03 13:44:09 +0200

    fixed untunnel

--- test.cpp
+++ test.cpp
@@ -16,10 +16,11 @@ string untunnel(tfm_index &tfm) {
     string original(tfm.size(), ' ');
 
     auto p = tfm.end();
-    for (tfm_index::size_type i = 0; i < tfm.size(); i++) {
+    for (tfm_index::size_type i = 1; i < tfm.size(); i++) {
         char c = (char)tfm.backwardstep(p);
         original[tfm.size() - i - 1] = c;
     }
+    original[tfm.size() - 1] = (char)tfm.preceding_char(p);
 
     return original;
 }
@@ -30,7 +31,7 @@ int main() {
     bit_vector in{1,1,1,1,1,1,1,1,1,1,1,1,1};
     bit_vector out{1,1,1,1,1,1,1,1,1,1,1,1,1};
 
-    cout << "L size: " << L.size() << "\tL width: " << (size_t)L.width() << endl;
+    // cout << "L size: " << L.size() << "\tL width: " << (size_t)L.width() << endl;
 
     tfm_index tfm(size, L, in, out);
     cout << untunnel(tfm) << endl;
